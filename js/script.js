e3ccfa8835c346378ba8956b06460f966e05814c
Njoki <kinuthiash@gmail.com>
2022-04-20 14:29:46 +0300

    feat:fetch from weather api

--- js/script.js
+++ js/script.js
@@ -12,6 +12,16 @@ const apiKey = `28d519b791ec96557c38884a192d1bc7`;
 button.addEventListener("click", (e) => {
   e.preventDefault();
   console.log(cityInput.value)
-  // fetchWeatherData()
-  // displayData()
+  fetchWeatherData(60.99, 30.9, apiKey)
 })
+
+// fetch data functions
+// weather data
+function fetchWeatherData(lat, lon, key) {
+  fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon +
+    "&appid=" + key)
+    .then(resp => resp.json())
+    .then(weatherInfo => console.log(weatherInfo))
+    .catch((err) => alert(err.message))
+}
+
