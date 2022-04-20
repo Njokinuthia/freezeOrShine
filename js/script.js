7230c387029496524241d5117c409166ed7ae2ea
Njoki <kinuthiash@gmail.com>
2022-04-20 14:19:03 +0300

    add button functionality

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
-const heading  = document.querySelector("h1#heading")
-heading.style.color = "blue";
+// element selection from dom
+const cityInput = document.querySelector("input.cityInput")
+const button = document.querySelector("input.button")
+const name = document.querySelector(".name")
+const description = document.querySelector(".desc")
+const temperature = document.querySelector(".temp")
+
+// weather app api key
+const apiKey = `28d519b791ec96557c38884a192d1bc7`;
+
+// button functionality - event listener
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log(cityInput.value)
+  // fetchWeatherData()
+  // displayData()
+})
