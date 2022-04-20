356de1add0ef28b55d66062e4a0b2bbf1bf3ffcd
Njoki <kinuthiash@gmail.com>
2022-04-20 22:46:02 +0300

    feat:update display api data functionality

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
 // element selection from dom
-const cityInput = document.querySelector("input.cityInput")
+const city = document.querySelector("input.cityInput")
 const button = document.querySelector("input.button")
-const name = document.querySelector(".name")
-const description = document.querySelector(".desc")
-const temperature = document.querySelector(".temp")
+const form = document.querySelector("form")
+
 
 // weather app api key
 const apiKey = `28d519b791ec96557c38884a192d1bc7`;
@@ -11,21 +10,27 @@ const apiKey = `28d519b791ec96557c38884a192d1bc7`;
 // button functionality - event listener
 button.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log(cityInput.value)
-  fetchWeatherData(cityInput.value , apiKey)
-  // geoLocation(cityInput.value)
-
+  console.log(city.value)
+  fetchWeatherData(city.value, apiKey)
 })
 
 // fetch data functions
-function fetchWeatherData(city,apiKey) {  
-  fetch("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+apiKey)
+const fetchWeatherData = (city, apiKey) => {
+  fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=metric")
     .then(resp => resp.json())
-    .then(weatherInfo => console.log(weatherInfo))
+    .then(weatherInfo => {
+      console.log(weatherInfo)
+      console.log(weatherInfo.weather[0].main)
+      console.log(weatherInfo.weather[0].description)
+      console.log(weatherInfo.main.temp)
+      displayWeather(weatherInfo)
+      form.reset();
+    })
     .catch((err) => alert(err.message))
+    
 }
-
-
-
-
-
+function displayWeather(weatherInfo){
+  document.querySelector(".name").innerHTML = `City: ${weatherInfo.name}`;
+  document.querySelector(".desc").innerHTML = `Weather: ${weatherInfo.weather[0].main}`;
+  document.querySelector(".temp").innerHTML = `Temp: ${weatherInfo.main.temp}`
+}
