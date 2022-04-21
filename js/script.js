3d6e0f4249a041979f46c279d19fb573448c4626
Njoki <kinuthiash@gmail.com>
2022-04-21 15:27:32 +0300

    feat:change background according to weather

--- js/script.js
+++ js/script.js
@@ -9,8 +9,8 @@ const apiKey = `28d519b791ec96557c38884a192d1bc7`;
 
 // button functionality - event listener
 button.addEventListener("click", (e) => {
-  e.preventDefault();
-  // console.log(city.value)
+  e.preventDefault();  
+ 
   fetchWeatherData(city.value, apiKey)
 })
 
@@ -18,24 +18,25 @@ button.addEventListener("click", (e) => {
 const fetchWeatherData = (city, apiKey) => {
   fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=metric")
     .then(resp => resp.json())
-    .then(weatherInfo => {
-      // console.log(weatherInfo)
-      // console.log(weatherInfo.weather[0].icon)
-      // console.log(weatherInfo.weather[0].icon)
-      // console.log(weatherInfo.weather[0].description)
-      // console.log(weatherInfo.main.temp)
+    .then(weatherInfo => {     
       displayWeather(weatherInfo)
+      // document.querySelector(".display").classList.add("display:after")
+      // document.querySelector(".display").classList.remove("display")
       form.reset();
+
+      document.body.style.backgroundImage = "url(https://source.unsplash.com/1600x900/?"+weatherInfo.weather[0].main+")"
     })
-    .catch((err) => alert(err.message))
-    
+    .catch((err) => alert(err.message))    
 }
+// display weather
 function displayWeather(weatherInfo){
-  document.querySelector(".name").innerHTML = `Weather in ${weatherInfo.name}`;
+  document.querySelector(".name").innerHTML = ` ${weatherInfo.name}`;
   document.querySelector(".desc").innerHTML = `${weatherInfo.weather[0].main}`;
-  document.querySelector(".temp").innerHTML = `Temp: ${weatherInfo.main.temp} °C`;
+  document.querySelector(".temp").innerHTML = `${weatherInfo.main.temp}°C`;
   document.querySelector(".weather-icon").src ="https://openweathermap.org/img/wn/"+weatherInfo.weather[0].icon+"@2x.png";
-  document.querySelector(".humidity").innerHTML = `Humidity: ${weatherInfo.main.humidity} %`
+  document.querySelector(".humidity").innerHTML = `Humidity: ${weatherInfo.main.humidity}%`
   document.querySelector(".pressure").innerHTML = `Pressure: ${weatherInfo.main.pressure}`
-  document.querySelector(".wind").innerHTML = `Wind: ${weatherInfo.wind.speed} km/h`
+  document.querySelector(".wind").innerHTML = `Wind: ${weatherInfo.wind.speed}km/h`
 }
+
+// hide on search
